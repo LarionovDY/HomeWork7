6c87960c7899b7ccd8ff20e2d1c7f6b0618f5cc5
Dmitriy Larionov <larionov.d.y@gmail.com>
2021-11-20 18:46:29 +0300

    Добавьте файлы проекта.

---
+++ HomeWork7_1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork7_1
+{
+    //Два треугольника заданы длинами своих сторон.Определить, площадь какого из них больше (создать метод для вычисления площади  треугольника по длинам его сторон).
+    //Для решения задачи можно использовать формулу Герона.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите пожалуйста длины сторон первого треугольника:");
+            Console.WriteLine();
+            Console.WriteLine("Сторона 1:");
+            double triangleSide1_1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Сторона 2:");
+            double triangleSide1_2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Сторона 3:");
+            double triangleSide1_3 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите пожалуйста длины сторон второго треугольника:");
+            Console.WriteLine();
+            Console.WriteLine("Сторона 1:");
+            double triangleSide2_1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Сторона 2:");
+            double triangleSide2_2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Сторона 3:");
+            double triangleSide2_3 = Convert.ToDouble(Console.ReadLine());
+            double triangleArea1 = TriangleArea(triangleSide1_1, triangleSide1_2, triangleSide1_3);
+            double triangleArea2 = TriangleArea(triangleSide2_1, triangleSide2_2, triangleSide2_3);
+            CompareArea(triangleArea1, triangleArea2);
+            Console.ReadKey();
+        }
+        static double TriangleArea(double Side1, double Side2, double Side3)    //метод, вычисляющий площадь треугольника
+        {
+            double halfPerimeter = (Side1 + Side2 + Side3) / 2;
+            double Area = Math.Sqrt(halfPerimeter * (halfPerimeter - Side1) * (halfPerimeter - Side2) * (halfPerimeter - Side3));
+            return Area;
+        }
+        static void CompareArea(double Area1, double Area2)     // метод, сравнивающий площади двух треугольников и выводящий ответ на консоль
+        {
+            Console.WriteLine("Наибольшая площадь у треугольника № {0}, его площадь составляет: {1:f2}, площадь меньшего треугольника: {2:f2}",
+                Area1 > Area2 ? 1 : 2, Area1 > Area2 ? Area1 : Area2, Area1 > Area2 ? Area2 : Area1);
+        }
+    }
+}
---
+++ HomeWork7_2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork7_2
+{
+    //Создать метод для вычисления объема и площади поверхности куба по длине его ребра.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите пожалуйста длину стороны куба");
+            double side, volume, area;
+            side = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+            Cube(side, out volume, out area);
+            Console.WriteLine("Объем куба со стороной {0:f2}:\t{1:f2}\nплощадь поверхности:\t{2:f2}",  side, volume, area);
+            Console.ReadKey();
+        }
+        static void Cube(double cubeSide, out double cubeVolume, out double cubeSurfaceArea) 
+        {
+            cubeVolume = Math.Pow(cubeSide, 3);
+            cubeSurfaceArea = 6 * Math.Pow(cubeSide, 2);
+        }
+    }
+}
